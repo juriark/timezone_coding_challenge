84e9370861f02a55dbbcdc1815ac10bad35ed24d
Julia Sammer <juliasammer@gmx.at>
2023-04-24 11:04:41 +0200

    Fix writing timezones to database, and race condition in container spinup

--- src/timezones/database.py
+++ src/timezones/database.py
@@ -3,7 +3,10 @@ from pathlib import Path
 import typing as t
 
 import geopandas
-from sqlalchemy import create_engine, engine
+from geoalchemy2 import Geometry
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.sql import text
 from sqlalchemy.orm import sessionmaker
 
 from timezones.constants import INPUT_FILE, Status, CONNECTION_STRING
@@ -11,7 +14,7 @@ from timezones.constants import INPUT_FILE, Status, CONNECTION_STRING
 _logger = logging.getLogger(__name__)
 
 engine = create_engine(CONNECTION_STRING, echo=True)
-Session = sessionmaker(engine)  # use sessionmaker to avoid passing around engine object
+Session = sessionmaker(engine)
 
 
 class TimezoneDB:
@@ -24,8 +27,8 @@ class TimezoneDB:
     def __exit__(self, *args: t.Any) -> None:
         self.session.close()
 
-    def _get_engine(self) -> engine:
-        """Get sqlachemy engine from current Session"""
+    def _get_engine(self) -> Engine:
+        """Get sqlachemy engine object from current session"""
         return self.session.get_bind()
 
     def create_table_from_shapefile(
@@ -57,9 +60,37 @@ class TimezoneDB:
 
         # Write to database
         try:
-            gdf.to_postgis(name=table_name, con=self._get_engine(), schema=schema)
+            gdf.to_postgis(name=table_name, con=self._get_engine(), schema=schema,
+                           dtype={'geometry': Geometry('POLYGON', srid=4326)})
         except ValueError as error:
             _logger.error(f"Failed to write dataframe to database: {error}")
             return Status.FAIL
 
+        _logger.info("Successfully written to database.")
         return Status.SUCCESS
+
+
+if __name__ == '__main__':
+    # This script is being called when spinning up the docker network from `docker compose`, and effectively writes the
+    # timezones to the database.
+
+    from time import sleep
+
+    # Initial database setup
+    _logger.info(f"Trying to write timezone table to database {CONNECTION_STRING}")
+
+    # Might run into race condition for first container spinup.
+    num_tries = 0
+    max_tries = 4
+    while num_tries <= max_tries:
+        try:
+            with TimezoneDB() as db:
+                write_to_db_status = db.create_table_from_shapefile(
+                    rows=10)  # TODO: write sqlalchemy schema for timezones
+                break
+        except OperationalError:
+            if num_tries == max_tries:
+                _logger.error("Cannot connect to database - number of tries exceeded")
+                raise
+            num_tries += 1
+            sleep(5)
